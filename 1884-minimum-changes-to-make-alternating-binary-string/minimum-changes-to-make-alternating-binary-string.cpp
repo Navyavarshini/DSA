702f9313ae119729c75de52a768530640fefcc88
Navyavarshini <155437996+Navyavarshini@users.noreply.github.com>
2026-03-05 08:52:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (99.78%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int minOperations(string s) {
-         char c_0 = s[0];
-        int count1 = count(s, c_0);
-        int count2 = count(s, c_0 == '0' ? '1' : '0') + 1;
-        return min(count1, count2);
-    }
-
-private:
-    int count(string s, char c_pre) {
-        int count = 0;
-        for (int i = 1; i < s.length(); i++) {
-            char current = s[i];
-            if (current == c_pre) {
-                count++;
-                c_pre = c_pre == '0' ? '1' : '0';
-            } else {
-                c_pre = current;
-            }
-        }
-        return count;
+    int minOperations(string& s) {
+        //op[0] count op need for "0101.." 
+        //op[1] count op need for "1010.."
+        int n=s.size(), op[2]={0};
+        for(int i=0; i<n; i+=2)
+            op[s[i]=='0']++;
+            
+        for (int i=1; i<n; i+=2)
+            op[s[i]=='1']++;
+        return min(op[0], op[1]);
     }
 };
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
